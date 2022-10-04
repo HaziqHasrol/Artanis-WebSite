88f4feb75b6b3d3022fce1189eea1ac8dcf64427
HaziqHasrol <haziqhasrol@gmail.com>
2022-10-04 15:21:03 +0800

    .

--- test4.php
+++ test4.php
@@ -91,32 +91,15 @@ body {
   color: rgba(255, 255, 255, 0.9);
 }
 
-.card__link::after {
-  position: absolute;
-  top: 25px;
-  left: 0;
-  content: "";
-  width: 0%;
-  height: 3px;
-  background-color: rgba(255, 255, 255, 0.6);
-  transition: all 0.5s;
-}
 
-.card__link:hover::after {
-  width: 100%;
-}
 
-.card__icon {
-  grid-row: 2/3;
-  font-size: 20px;
-}
 
 .card__title {
   grid-row: 3/4;
   font-weight: 400;
   color: #ffffff;
   font-weight: bold;
-  text-shadow:1px 1px black;
+  text-shadow:1px 2px black;
   padding-top:30px;
 }
 
@@ -185,106 +168,6 @@ border-radius: 10%;
   font-size:70px;
 }
 
-/* button glitch*/
-.button-49,
-.button-49:after {
-  width: 250px;
-  height: 100px;
-  line-height: 100px;
-  font-size: 11px;
-  font-family: 'Bebas Neue', sans-serif;
-  font-weight: bolder;
-  background: linear-gradient(45deg, transparent 10%, #e4e0d4 10%);
-  color: black;
-  letter-spacing: 1px;
-  outline: transparent;
-  border: outset ;
-  position: relative;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-}
-
-.button-49:after {
-  --slice-0: inset(50% 50% 50% 50%);
-  --slice-1: inset(80% -6px 0 0);
-  --slice-2: inset(50% -6px 30% 0);
-  --slice-3: inset(10% -6px 85% 0);
-  --slice-4: inset(40% -6px 43% 0);
-  --slice-5: inset(80% -6px 5% 0);
-  
-  content: 'ALTERNATE TEXT';
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: linear-gradient(45deg, transparent 8%, #00E6F6 10%, #00E6F6 10%, #e4e0d4 10%);
-  text-shadow: -3px -3px 0px red, 3px 3px 0px #00E6F6;
-  clip-path: var(--slice-0);
-}
-
-.button-49:hover:after {
-  animation: 1s glitch;
-  animation-timing-function: steps(2, end);
-}
-
-@keyframes glitch {
-  0% {
-    clip-path: var(--slice-1);
-    transform: translate(-20px, -10px);
-  }
-  10% {
-    clip-path: var(--slice-3);
-    transform: translate(10px, 10px);
-  }
-  20% {
-    clip-path: var(--slice-1);
-    transform: translate(-10px, 10px);
-  }
-  30% {
-    clip-path: var(--slice-3);
-    transform: translate(0px, 5px);
-  }
-  40% {
-    clip-path: var(--slice-2);
-    transform: translate(-5px, 0px);
-  }
-  50% {
-    clip-path: var(--slice-3);
-    transform: translate(5px, 0px);
-  }
-  60% {
-    clip-path: var(--slice-4);
-    transform: translate(5px, 10px);
-  }
-  70% {
-    clip-path: var(--slice-2);
-    transform: translate(-10px, 10px);
-  }
-  80% {
-    clip-path: var(--slice-5);
-    transform: translate(20px, -10px);
-  }
-  90% {
-    clip-path: var(--slice-1);
-    transform: translate(-10px, 0px);
-  }
-  100% {
-    clip-path: var(--slice-1);
-    transform: translate(0);
-  }
-}
-
-@media (min-width: 768px) {
-  .button-49,
-  .button-49:after {
-    width: 200px;
-    height: 86px;
-    line-height: 88px;
-  }
-}
 
 
         
@@ -415,8 +298,4 @@ border-radius: 10%;
 </div>
 
     </body>
-
-    
-    
-
 </html>
